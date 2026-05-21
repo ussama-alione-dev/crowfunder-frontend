85377c928fecbe0ce4be35b5638db69a0b97a78d
alione10-lm <aliouaneoussama89@gmail.com>
2026-05-21 11:56:09 +0100

    feat: enhance login functionality with loading state management

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const user = localStorage.getItem("user");
+const token = localStorage.getItem("token");
+
 const authSlice = createSlice({
     name: "auth",
     initialState: {
-        user: null,
-        token: null,
+        user: user ? JSON.parse(user) : null,
+        token: token || null,
+        loading: false,
     },
     reducers: {
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+
         login: (state, action) => {
             state.user = action.payload.user;
             state.token = action.payload.token;
@@ -15,9 +23,12 @@ const authSlice = createSlice({
         logout: (state) => {
             state.user = null;
             state.token = null;
+
+            localStorage.removeItem("user");
+            localStorage.removeItem("token");
         },
     },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, setLoading } = authSlice.actions;
 export default authSlice.reducer;
